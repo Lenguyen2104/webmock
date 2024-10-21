c2a1ba83e90de7f54cf0ea27baf6181f0eb860a6
letannguyen <letannguyen210496@gmail.com>
2024-10-21 15:40:36 +0700

    fix color

--- src/config/connectDB.js
+++ src/config/connectDB.js
@@ -1,15 +1,15 @@
 const mysql = require("mysql2/promise");
 
-// const connection = mysql.createPool({
-//   host: "localhost",
-//   user: "root",
-//   password: "",
-//   database: "wingo68",
-// });
 const connection = mysql.createPool({
-  host: "103.68.109.38",
-  user: "wingo",
-  password: "wingo",
-  database: "wingo",
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "wingo68",
 });
+// const connection = mysql.createPool({
+//   host: "103.68.109.38",
+//   user: "wingo",
+//   password: "wingo",
+//   database: "wingo",
+// });
 export default connection;
